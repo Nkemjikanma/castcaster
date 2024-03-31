cf9d80a5d77b6296a97e7b0d2ffd9fbb6185796b
nkemjika <nkemjikaohanyere@gmail.com>
2024-03-31 17:09:44 +0100

    feat: add loading

---
+++ src/app/search/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+    return (
+        <ul className="divide-y divide-gray-200 rounded-b-md bg-white shadow-md">
+            <div className="h-10 w-1/2 rounded bg-gray-300 " />
+            <div className="space-y-2">
+                <div className="h-4 w-4/5 rounded bg-gray-300" />
+                <div className="h-4 w-4/5 rounded bg-gray-300" />
+                <div className="h-4 w-4/5 rounded bg-gray-300" />
+            </div>
+        </ul>
+    );
+}
