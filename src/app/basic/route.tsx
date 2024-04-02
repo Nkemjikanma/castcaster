d566ed3b45fb9e7048bda9dd8b6002b005b6baf8
nkemjika <nkemjikaohanyere@gmail.com>
2024-04-02 02:55:46 +0100

    style: style fixes

--- src/app/basic/route.tsx
+++ src/app/basic/route.tsx
@@ -16,9 +16,9 @@ export async function GET(request: Request) {
 
         // ?description=
         const hasDescription = searchParams.has("description");
-        const description = searchParams.get("description");
-
-        const descriptionPostion = hasDescription ? undefined : "center";
+        const description = hasDescription
+            ? searchParams.get("description")
+            : "Generate casts like warpcaster users";
 
         return new ImageResponse(
             (
@@ -57,7 +57,7 @@ export async function GET(request: Request) {
                                 marginTop: 30,
                                 color: "white",
                                 width: "80%",
-                                justifyContent: descriptionPostion,
+                                justifyContent: "center",
                             }}
                         >
                             {description}
