faf4861c2d27ac6ca949f6483ed943196afc5ac7
nkemjika <nkemjikaohanyere@gmail.com>
2024-04-03 00:32:40 +0100

    feat: fix error

--- src/app/(frame)/share/[searchParams]/page.tsx
+++ src/app/(frame)/share/[searchParams]/page.tsx
@@ -16,15 +16,23 @@ export default function Page({ params }: SharePageProps) {
             <head>
                 <title>{searchParams.title}</title>
                 <meta property="og:title" content={searchParams.title} />
-                <meta property="og:image" content={`http://castcaster.vercel.app/share?${searchParams}`} />
+                <meta
+                    property="og:image"
+                    content={`http://castcaster.vercel.app/share?${searchParams}`}
+                />
                 <meta name="fc:frame" content="vNext" />
                 <meta name="fc:frame:image:aspect_ratio" content="1:1" />
-                <meta name="fc:frame:image" content={`http://castcaster.vercel.app/share?${searchParams}`} />
-                <meta name="fc:frame:post_url" content={`${process.env.BASE_URL}/api/cast`} />
+                <meta
+                    name="fc:frame:image"
+                    content={`http://castcaster.vercel.app/share?${searchParams}`}
+                />
+                <meta
+                    name="fc:frame:post_url"
+                    content={`${process.env.BASE_URL}/api/cast`}
+                />
                 <meta name="fc:frame:button:1" content="Generate cast" />
                 <meta name="fc:frame:button:1:action" content="post" />
             </head>
         </html>
     );
-}
 }
