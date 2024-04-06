0163b909317f8e81888e1b1fe11b80c96350851a
nkemjika <nkemjikaohanyere@gmail.com>
2024-04-06 12:01:44 +0100

    style: remove blue debug border

--- src/app/(images)/display/route.tsx
+++ src/app/(images)/display/route.tsx
@@ -80,7 +80,6 @@ export async function GET(request: Request) {
                                 flexDirection: "column",
                                 margin: "0",
                                 justifyContent: "center",
-                                border: "2px solid blue",
                             }}
                         >
                             <p
