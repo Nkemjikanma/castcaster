4b362faa223eeae2ea894fb7d0a2c444f6296894
nkemjika <nkemjikaohanyere@gmail.com>
2024-04-07 19:42:52 +0100

    fix: fixes

--- src/app/(frame)/share/[searchParams]/page.tsx
+++ src/app/(frame)/share/[searchParams]/page.tsx
@@ -27,9 +27,8 @@ export default function Page({ params }: SharePageProps) {
                 />
                 <meta
                     name="fc:frame:post_url"
-                    content={`${process.env.NEXT_PUBLIC_HOST_URL}/api/cast`}
+                    content={`${process.env.NEXT_PUBLIC_HOST_URL}`}
                 />
-                <meta name="fc:frame:input" content="Enter username of fid" />
                 <meta name="fc:frame:button:1" content="Generate a cast" />
                 <meta name="fc:frame:button:1:action" content="post" />
             </head>
