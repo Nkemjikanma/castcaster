b735149e1ec2d9b8d98bf07bec700e479ef0619e
nkemjika <nkemjikaohanyere@gmail.com>
2024-04-09 00:08:07 +0100

    refactor: dynamic search page

--- src/app/(frame)/share/[searchParams]/page.tsx
+++ src/app/(frame)/share/[searchParams]/page.tsx
@@ -13,19 +13,27 @@ interface SharePageProps {
 export default function Page({ params }: SharePageProps) {
     const { searchParams } = params;
 
-    const htmlOutput = getFrameHtmlResponse({
-        ogTitle: searchParams.title,
-        ogDescription: searchParams.description,
-        image: `https://castcaster.vercel.app/api/display?${searchParams}`,
-        buttons: [
-            {
-                label: "Generate a cast",
-                action: "post",
-                target: `${process.env.NEXT_PUBLIC_HOST_URL}`,
-            },
-        ],
-        postUrl: `${process.env.NEXT_PUBLIC_HOST_URL}`,
-    });
-
-    return { htmlOutput };
+    return (
+        <html>
+            <head>
+                <title>{searchParams.title}</title>
+                <meta property="og:title" content={searchParams.title} />
+                <meta
+                    property="og:image"
+                    content={`https://castcaster.vercel.app/api/display?${searchParams}`}
+                />
+                <meta name="fc:frame" content="vNext" />
+                <meta
+                    name="fc:frame:image"
+                    content={`https://castcaster.vercel.app/api/display?${searchParams}`}
+                />
+                <meta
+                    name="fc:frame:post_url"
+                    content={`${process.env.NEXT_PUBLIC_HOST_URL}`}
+                />
+                <meta name="fc:frame:button:1" content="Generate a cast" />
+                <meta name="fc:frame:button:1:action" content="post" />
+            </head>
+        </html>
+    );
 }
